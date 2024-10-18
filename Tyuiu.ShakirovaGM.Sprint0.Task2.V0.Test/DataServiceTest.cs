015a72be0f40ed46b1d6ad118849d670e58a9112
tyuiu.ShakirovaGM@outlook.com <tyuiu.ShakirovaGM@outlook.com>
2024-10-19 03:32:26 +0500

    Task2

---
+++ Tyuiu.ShakirovaGM.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShakirovaGM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ShakirovaGM.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Тюлень";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Тюлень", res);
+        }
+    }
+}
---
+++ Tyuiu.ShakirovaGM.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShakirovaGM.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ShakirovaGM.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Тюлень"));
+            Console.ReadKey();
+        }
+    }
+}
